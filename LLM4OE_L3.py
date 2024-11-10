5fafcdbce14057d70d8965c049ce385b3707d035
Andreas Soularidis <andreas.soularidis@gmail.com>
2024-11-10 16:37:53 +0200

    Retrievers refactoring completed

--- LLM4OE_L3.py
+++ LLM4OE_L3.py
@@ -28,7 +28,6 @@ SAR_DOCUMENTS = "data/SAR_docs_text"
 OWL_DOCUMENTATION = "https://www.w3.org/2007/OWL/draft/owl2-primer/#Classes.2C_Properties.2C_and_Individuals_.E2.80.93_And_Basic_Modeling_With_Them"
 REACT_DOCUMENTS = "data/ReAct-v2"
 
-model = ModelLoader.load_llm(MODEL, TEMPERATURE)
 
 def split_documents(docs, chunk_size, chunk_overlap):
   splitter = RecursiveCharacterTextSplitter(
@@ -44,11 +43,7 @@ def create_vector_store(docs):
   return vector_store
 
 
-def format_docs(docs):
-  return "\n\n".join(doc.page_content for doc in docs)
-
-
-def process_chat(question, sar_retriever, owl_retriever, react_retriever):
+def start_simulation(question, sar_retriever, owl_retriever, react_retriever):
     template = CustomPromptBuilder.build(MODE.value, question)
     custom_rag_prompt = PromptTemplate.from_template(template)
     rag_chain = Builder.create_rag_chain(MODE.value, sar_retriever, owl_retriever, react_retriever, model, custom_rag_prompt)
@@ -59,22 +54,29 @@ def process_chat(question, sar_retriever, owl_retriever, react_retriever):
 
 
 if __name__ == '__main__':
-  print("Initialization..")
-  sar_docs = FileLoader.get_txt_loader(SAR_DOCUMENTS)
-  owl_docs = FileLoader.get_web_loader(OWL_DOCUMENTATION)
-  react_docs = FileLoader.get_txt_loader(REACT_DOCUMENTS)
+  print("Initialization. Please wait..\n")
+  
+  model = ModelLoader.load_llm(MODEL, TEMPERATURE)
+  
+  react_retriever = owl_retriever = sar_retriever = None
 
-  sar_docs = split_documents(sar_docs, CHUNK_SIZE, CHUNK_OVERLAP)
-  owl_docs = split_documents(owl_docs, CHUNK_SIZE, CHUNK_OVERLAP)
-  react_docs = split_documents(react_docs, CHUNK_SIZE, CHUNK_OVERLAP)
-
-  sar_vector_store = create_vector_store(sar_docs)
-  owl_vector_store = create_vector_store(owl_docs)
-  react_vector_store = create_vector_store(react_docs)
-
-  sar_retriever = sar_vector_store.as_retriever(search_type=SEARCH_TYPE, search_kwargs={"k": K})
-  owl_retriever = owl_vector_store.as_retriever(search_type=SEARCH_TYPE, search_kwargs={"k":K})
-  react_retriever = react_vector_store.as_retriever(search_type=SEARCH_TYPE, search_kwargs={"k":K})
+  if MODE.value == 1 or MODE.value == 2 or MODE.value == 3: # ReAct
+    react_docs = FileLoader.get_txt_loader(REACT_DOCUMENTS)
+    react_docs = split_documents(react_docs, CHUNK_SIZE, CHUNK_OVERLAP)
+    react_vector_store = create_vector_store(react_docs)
+    react_retriever = react_vector_store.as_retriever(search_type=SEARCH_TYPE, search_kwargs={"k":K})
+  
+  if MODE.value == 1 or MODE.value == 2: # ReAct & Domain Data
+    sar_docs = FileLoader.get_txt_loader(SAR_DOCUMENTS)
+    sar_docs = split_documents(sar_docs, CHUNK_SIZE, CHUNK_OVERLAP)
+    sar_vector_store = create_vector_store(sar_docs)
+    sar_retriever = sar_vector_store.as_retriever(search_type=SEARCH_TYPE, search_kwargs={"k": K})
+  
+  if MODE.value == 1: # ReAct & Domain Data & OWL Documentation
+    owl_docs = FileLoader.get_web_loader(OWL_DOCUMENTATION)
+    owl_docs = split_documents(owl_docs, CHUNK_SIZE, CHUNK_OVERLAP)
+    owl_vector_store = create_vector_store(owl_docs)
+    owl_retriever = owl_vector_store.as_retriever(search_type=SEARCH_TYPE, search_kwargs={"k":K})
 
   filename = f"{MODEL}-{TEMPERATURE}-{CHUNK_SIZE}-{SEARCH_TYPE}-{K}-{MODE}-{ITERATION}-Test"
   ontology_extractor = OntologyExtractor("SAR/Level3/Phase_2/Ontologies", "Ontology-" + filename)
@@ -86,23 +88,12 @@ if __name__ == '__main__':
     
     if user_input.lower().strip() == "exit":
       break
-    
-    match MODE.value:
-      case 1:
-        response = process_chat(user_input, sar_retriever, owl_retriever, react_retriever) 
-      case 2:
-        response = process_chat(user_input, sar_retriever, None, react_retriever)
-      case 3:
-        response = process_chat(user_input, None, None, react_retriever)
-      case 4:
-        response = process_chat(user_input, None, None, None)
-      case 5:
-        response = process_chat(user_input, sar_retriever, None, None)
-      case _:
-        print("Error: No mode was selected")
+
+    response = start_simulation(user_input, sar_retriever, owl_retriever, react_retriever) 
 
     print(response)
 
     ontology_extractor.export_to_ttl(response)
+  
   chat_extractor.discussion_extractor(response)
 
